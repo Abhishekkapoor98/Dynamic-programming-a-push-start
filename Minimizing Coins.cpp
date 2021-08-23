acc736f31aaecd80aa9c01dc513bf332e2dce511
Abhishekkapoor98 <89387661+Abhishekkapoor98@users.noreply.github.com>
2021-08-23 16:08:15 +0530

    Add files via upload

---
+++ Minimizing Coins.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+#define fast_io ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lli long long int
+#define mod 1000000007
+#define PB push_back
+#define PPB pop_back
+#define MP make_pair
+#define vi vector<int>
+#define vli vector<lli>
+#define uos unordered_set<int>
+#define min_heap priority_queue<int,vector<int>,greater<int>>
+#define max_heap priority_queue<int>
+#define pii pair<int,int>
+#define F first
+#define S second
+#define loop(a,b) for(lli i=a;i<=b;i++)
+#define endl '\n'
+ 
+lli t[101][1000001];
+ 
+int minCoins(vi coins, int m, int V)
+{
+	int table[V+1];
+ 
+	table[0] = 0;
+ 
+	for (int i=1; i<=V; i++)
+		table[i] = INT_MAX;
+ 
+	for (int i=1; i<=V; i++)
+	{
+		for (int j=0; j<m; j++)
+		if (coins[j] <= i)
+		{
+			int sub_res = table[i-coins[j]];
+			if (sub_res != INT_MAX && sub_res + 1 < table[i])
+				table[i] = sub_res + 1;
+		}
+	}
+ 
+	if(table[V]==INT_MAX)
+		return -1;
+ 
+	return table[V];
+}
+ 
+void solve()
+{
+	vi coins;
+	lli n, sum;
+	cin>>n>>sum;
+ 
+	int temp;
+	while(n--)
+	{
+		cin>>temp;
+		coins.PB(temp);
+	}
+ 
+	n = coins.size();
+ 
+	cout<<minCoins(coins,n,sum);
+}
+ 
+int main()
+{
+	fast_io;
+ 
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+#endif
+ 
+	solve();
+	
+	return 0;
+}
